2269420cbd8df3a5ac134cc8166c96e43df8511d
Max-Hi <levin.hirsch@web.de>
2023-10-03 15:06:47 +0100

    Datenschutz

---
+++ ui/index.js
@@ -0,0 +1,10 @@
+function changeVisibility(){
+    var div = document.getElementById("dataprotection")
+    
+    if (div.style.display === "none"){
+        div.style.display = "block";
+    }
+    else {
+        div.style.display = "none";
+    }
+}
