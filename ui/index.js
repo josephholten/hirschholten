dadfeac55a1e03fb52d18eadb8e5ebdeeda5053e
Joseph Holten <joseph@holten.com>
2023-10-04 13:59:58 +0200

    fixups in how popups work and underline 'über mich'

--- ui/index.js
+++ ui/index.js
@@ -1,4 +1,8 @@
-let visible = false;
+let visible = {
+    "aboutme-joseph": false,
+    "aboutme-max": false,
+    "dataprotection": false,
+};
 
 /* Extends Element Objects with a function named scrollIntoViewPromise
 *  options: the normal scrollIntoView options without any changes
@@ -37,13 +41,16 @@ Element.prototype.scrollIntoViewPromise = function(options){
 function changeVisibility(divname){
     var div = document.getElementById(divname)
     
-    if (!visible) {
+    if (!visible[divname]) {
         div.style.display = "block"
-        div.scrollIntoView({behavior: "smooth"})
+        if (divname === "dataprotection") 
+            div.scrollIntoView({behavior: "smooth"})
     } else {
-        document.getElementById("title").scrollIntoViewPromise({behavior: "smooth"})
-        .then(() => {div.style.display = "none"})
+        if (divname === "dataprotection") 
+            document.getElementById("title").scrollIntoViewPromise({behavior: "smooth"}).then(() => {div.style.display = "none"})
+        else
+            div.style.display = "none"
     }
 
-    visible = !visible
+    visible[divname] = !visible[divname]
 }
